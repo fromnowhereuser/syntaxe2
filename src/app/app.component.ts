3a45cbf2e1dae018f403377e68138399dd490f46
zerocodeautomation <53434708+zerocodeautomation@users.noreply.github.com>
2020-06-22 12:34:11 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'syntaxe';
+  public title: any;
+  public version = 1;
+  public srcImg = {
+    url: 'https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0?e=2159024400&v=beta&t=C7KMOtnrJwGrMXmgIk2u1B8a7VRfgxMwXng9cdP9kZk'
+  }
+
+  constructor() {
+    setTimeout(() => {
+      this.title = {
+        caption: 'syntaxe'
+      };
+    }, 1000);
+  }
+
+  public handleKeydown(event: KeyboardEvent) {
+    console.log(event);
+    
+  }
+
+
+  public handleClick(event: MouseEvent, input: HTMLInputElement): void {
+    console.log(event);
+    console.log(input);
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.log('clicked');
+  }
 }
