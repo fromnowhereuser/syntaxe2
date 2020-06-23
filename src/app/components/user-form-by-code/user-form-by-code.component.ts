d10a1b92ac4beb33cdd9b6fdf4e59e7aac81f26d
zerocodeautomation <53434708+zerocodeautomation@users.noreply.github.com>
2020-06-23 17:01:26 +0200

    form ok

--- src/app/components/user-form-by-code/user-form-by-code.component.ts
+++ src/app/components/user-form-by-code/user-form-by-code.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-form-by-code',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormByCodeComponent implements OnInit {
 
-  constructor() { }
+  @Output('usercreate')
+  usercreate = new EventEmitter<User>();
+
+  userForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.userForm = fb.group({
+      name: fb.control(''),
+      isAdmin: fb.control(false),
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    const user = new User('', false);
+    Object.assign(user, this.userForm.value);
+    this.usercreate.emit(user);
   }
 
 }
